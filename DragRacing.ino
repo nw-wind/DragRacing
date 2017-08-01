d35d0699de2cc7a30b6720d75233f0f638e82225
Sergey Keler <sergei@keler.net>
2017-08-01 16:46:10 +0300

    saved

--- DragRacing.ino
+++ DragRacing.ino
@@ -1,6 +1,8 @@
 #include "SmartDelay.h"
 #include "SmartButton.h"
 
+// светофор!! внешнее управление!! 1s
+
 template<class T> inline Print &operator <<(Print &obj, T arg) {
   obj.print(arg);
   return obj;
@@ -8,12 +10,14 @@ template<class T> inline Print &operator <<(Print &obj, T arg) {
 #define endl ("\n")
 
 #define KNOB  9
+#define INDICATOR 13
 
 struct player {
   volatile unsigned long d; // distanse (mm)
   volatile unsigned long c; // rotation counter
   unsigned long t; // время дистанции ms
 } pl[2];
+struct player op[2]; // for calculations out of interrupt.
 byte pp[2] = {2, 3};
 #define PLAYERS (sizeof(pp)/sizeof(byte))
 
@@ -31,7 +35,11 @@ class Baton : public SmartButton {
   public:
     Baton(int p) : SmartButton(p) {}
     void onClick() {
-      if (!start) {
+      if (start) {
+        start = 0;
+        Serial.println("STOP");
+        digitalWrite(INDICATOR,LOW);
+      } else {
         start = 1;
         idleTime = millis();
         for (int i = 0; i < PLAYERS; i++) {
@@ -40,6 +48,7 @@ class Baton : public SmartButton {
           pl[0].t = 0L;
         }
         Serial.println("START");
+        digitalWrite(INDICATOR,HIGH);
       }
     }
 };
@@ -59,6 +68,7 @@ void setup() {
   for (int i = 0; i < PLAYERS; i++) {
     pinMode(pp[i], INPUT_PULLUP);
   }
+  pinMode(INDICATOR,OUTPUT);
   attachInterrupt(digitalPinToInterrupt(pp[0]), getInt0, RISING);
   attachInterrupt(digitalPinToInterrupt(pp[1]), getInt1, RISING);
   Serial.println("READY");
@@ -69,28 +79,30 @@ void loop() {
   bt.run();
   if (start) {
     // Считаем время и расстояние
+    unsigned long sc[2]; // saved counters
     for (int i = 0; i < PLAYERS; i++) {
-      pl[i].d = pl[i].c * circle;
-      if (pl[i].c <= rotations) pl[i].t = millis() - idleTime;
+      sc[i]=pl[i].c;
+      pl[i].d = sc[i] * circle;
+      if (sc[i] <= rotations) pl[i].t = millis() - idleTime;
     }
     // Выводим в компорт
     if (disp.Now()) {
       for (int i = 0; i < PLAYERS; i++) {
-        if (pl[i].c > rotations) {
+        if (sc[i] > rotations) {
           Serial << pl[i].t << " FINISH ";
         } else
-          Serial << pl[i].t << " " << pl[i].d << " ";
+          Serial << pl[i].t << " " << pl[i].d / 1000UL << " ";
       }
       Serial.println();
     }
     // Если оба финишировали, заканчиваем.
-    if (pl[0].c > rotations && pl[1].c > rotations) {
+    if (sc[0] > rotations && sc[1] > rotations) {
       start = 0;
       for (int i = 0; i < PLAYERS; i++) {
         Serial << pl[i].t << " FINISH ";
       }
       Serial.println();
-
+      Serial.println("STOP");
     }
   }
 }
--- test.py
+++ test.py
@@ -1,45 +1,96 @@
 #!/usr/bin/python
+# coding: utf8
 
 from Tkinter import *
 from tkFont import *
 import serial
 import time
+import re
+import codecs
 
 def BexitCallBack():
 	exit()
 
+def readSerial():
+	ln=ser.readline().rstrip()
+	print "Read: '"+ln+"'"
+	if ln != '':
+        	if ln == 'READY':
+			bottomText.set(u"Готов!")
+			print "Ready to race"
+		if ln == 'START':
+			bottomText.set(u"Старт!")
+			print "Start!"
+		if ln == 'STOP':
+			bottomText.set(u"Гонка остановлена")
+			print "Stop!"
+		m = re.match('(\d+) (\w+) (\d+) (\w+)',ln)
+		if m:
+			pA=m.group(1,2)
+			pB=m.group(3,4)
+			leftText.set("%03d.%03d" % (int(pA[0])/1000,int(pA[0])%1000))
+			if pA[1] == 'FINISH':
+				SleftVar=402
+				# finish
+			else:
+			 	SleftVar=pA[1]
+			rightText.set("%03d.%03d" % (int(pB[0])/1000,int(pB[0])%1000))
+			if pB[1] == 'FINISH':
+				SrightVar=402
+				# finish
+			else:
+				SrightVar=pB[1]
+			Sleft.set(SleftVar)
+			Sright.set(SrightVar)
+			print "Arr= "+SleftVar+" ["+",".join(map(str,pA))+"], "+SrightVar+" ["+",".join(map(str,pB))+"]"
+		top.update()
+	top.after(50,readSerial)
+	
+
+
+ser = serial.Serial('/dev/tty.usbserial-A50285BI', 115200, timeout=1)
 top = Tk()
+top.geometry("800x600")
+helv36 = Font(family="Helvetica",size=36,weight="bold")
 m1 = PanedWindow(top, orient=VERTICAL)
 m1.pack(fill=BOTH, expand=1)
+mUp = PanedWindow(m1, orient=HORIZONTAL)
+m1.add(mUp)
 m2 = PanedWindow(m1, orient=HORIZONTAL)
 m1.add(m2)
-helv36 = Font(family="Helvetica",size=36,weight="bold")
-bottom = Label(m1, text="bottom pane",anchor=W)
+
+upLeftText=StringVar()
+upLeftText.set("Трек А")
+upLabelLeft= Label(mUp, textvariable=upLeftText)
+mUp.add(upLabelLeft)
+
+upRightText=StringVar()
+upRightText.set("Трек Б")
+upLabelRight= Label(mUp, textvariable=upRightText)
+mUp.add(upLabelRight)
+
+bottomText=StringVar()
+bottom = Label(m1, textvariable=bottomText, anchor=W)
+bottomText.set(u"Ожидание подключения")
 m1.add(bottom)
 Bexit = Button(top, text ="Exit", command = BexitCallBack)
 m1.add(Bexit)
-Lleft = Label(m2, text="left pane")
+leftText=StringVar()
+leftText.set("000.000")
+Lleft = Label(m2, textvariable=leftText)
 m2.add(Lleft)
-SleftVar=50
-Sleft = Scale(m2, orient=VERTICAL, variable=SleftVar, to=0, length=400, from_=400, digits=3, font=helv36)
+SleftVar=0
+Sleft = Scale(m2, orient=VERTICAL, to=0, length=402, from_=402, digits=3, font=helv36, width=50, bg="red")
 m2.add(Sleft)
-SrightVar=100
-Sright = Scale(m2, orient=VERTICAL, variable=SrightVar, to=0, length=400, from_=400, digits=3, font=helv36)
+SrightVar=0
+Sright = Scale(m2, orient=VERTICAL, to=0, length=402, from_=402, digits=3, font=helv36, width=50, bg="blue")
 m2.add(Sright)
-Lright = Label(m2, text="right pane")
+rightText=StringVar()
+rightText.set("000.000")
+Lright = Label(m2, textvariable=rightText)
 m2.add(Lright)
-Sleft.set(50)
-Sright.set(100)
+top.after(50,readSerial)
 top.mainloop()
 
 
-# https://petrimaki.com/2013/04/28/reading-arduino-serial-ports-in-windows-7/
-#ser = serial.Serial('COM3', 9600, timeout=0)
  
-#while 1:
-# try:
-#  print ser.readline()
-#  time.sleep(1)
-# except ser.SerialTimeoutException:
-#  print('Data could not be read')
-#  time.sleep(1)
