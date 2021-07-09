681f96d9129f216a44553816c39e39dbaca6da90
skiller <sergei@keler.net>
2021-07-09 13:53:50 +0300

    qr gui

--- DragRacing.py
+++ DragRacing.py
@@ -1,7 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
 from PyQt5 import QtWidgets, uic
-import sys
- 
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
+import sys, time, random
+
+# МКА
+
+
+
+# Классы
+
+class Worker(QObject):
+    finished = pyqtSignal()
+    progress = pyqtSignal(int, int)
+
+    def run(self):
+        print("in thread")
+        win.startButton.setText("Поехали...")
+        print("button")
+        ldist=0
+        rdist=0
+        for i in range(100):
+            print(f"t={i}")
+            ldist += random.randint(0,4)
+            rdist += random.randint(0,5)
+            self.progress.emit(ldist, rdist)
+            print("emited")
+            time.sleep(0.2)
+            print("wakeup")
+        print("look ends")
+        self.finished.emit()
+        print("finished")
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__() 
+        uic.loadUi('DragRacing.ui', self)
+        self.startButton.clicked.connect(self.runLongTask)
+        self.showMaximized()
+
+    def runLongTask(self):
+        self.thread = QThread()
+        self.worker = Worker()
+        self.worker.moveToThread(self.thread)
+        print("run!")
+        self.thread.started.connect(self.worker.run)
+        print("worker run!")
+        self.worker.finished.connect(self.thread.quit)
+        print("t q")
+        self.worker.finished.connect(self.worker.deleteLater)
+        print("w dl")
+        self.thread.finished.connect(self.thread.deleteLater)
+        print("t dl")
+        self.worker.progress.connect(self.reportProgress)
+        print("reportProgress")
+        self.thread.start()
+        print("started")
+        self.startButton.setEnabled(False)
+        self.thread.finished.connect(
+            lambda: self.startButton.setEnabled(True)
+        )
+        self.thread.finished.connect(
+            lambda: self.startButton.setText("Старт!")
+        )
+        # Раскраска фамилий.
+        # Запись отчёта.
+
+    def reportProgress(self,n, m):
+        print(f"repo {n}")
+        self.leftBar.setValue(n)
+        self.leftSpeed.setText(str(n%100))
+        self.leftDistance.setText(str(n))
+        self.rightBar.setValue(m)
+        self.rightSpeed.setText(str(m%100))
+        self.rightDistance.setText(str(m))
+        print(f"reported")
+
 app = QtWidgets.QApplication([])
-win = uic.loadUi("DragRacing.ui")
-win.show()
+win = Ui()
+# Надо нажать New... или заполнить поля.
+win.startButton.setEnabled(True)
+win.stopButton.setEnabled(False)
+# Пыщ!
 sys.exit(app.exec())
