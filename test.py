6c5cadbc4a747ad1b96c1e16c794464af3823d46
skiller <sergei@keler.net>
2021-09-02 11:32:37 +0400

    test

--- test.py
+++ test.py
@@ -9,18 +9,19 @@ GPIO.setup(21, GPIO.OUT)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-led=0
+led23=0
+led24=0
 def test_callback_23(channel):
-    global led
-    print(f'Event {led} detected for {channel}')
-    led+=1
-    GPIO.output(20,led%2)
+    global led23
+    print(f'Event {led23} detected for {channel}')
+    led23+=1
+    GPIO.output(20,led23%2)
 
 def test_callback_24(channel):
-    global led
-    print(f'Event {led} detected for {channel}')
-    led+=1
-    GPIO.output(21,led%2)
+    global led24
+    print(f'Event {led24} detected for {channel}')
+    led24+=1
+    GPIO.output(21,led24%2)
 
 GPIO.add_event_detect(23, GPIO.FALLING, callback=test_callback_23, bouncetime=250)
 GPIO.add_event_detect(24, GPIO.RISING, callback=test_callback_24, bouncetime=250)
